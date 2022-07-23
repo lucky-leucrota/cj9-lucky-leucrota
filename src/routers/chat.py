9ef688214b754a143bbd5fc4d5d758fca5ca22ca
Sasen Perera <sasen.learnings@gmail.com>
2022-07-23 21:18:34 +0530

    fixed somethings

--- src/routers/chat.py
+++ src/routers/chat.py
@@ -2,10 +2,24 @@ from fastapi import APIRouter, WebSocket, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+import random
+import string
+
 router = APIRouter()
 
 templates = Jinja2Templates(directory="src/templates")
 
+alphabets = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+
+def caesar(text, step, alphabets):
+    def shift(alphabet):
+        return alphabet[step:] + alphabet[:step]
+
+    shifted_alphabets = tuple(map(shift, alphabets))
+    joined_aphabets = ''.join(alphabets)
+    joined_shifted_alphabets = ''.join(shifted_alphabets)
+    table = str.maketrans(joined_aphabets, joined_shifted_alphabets)
+    return text.translate(table)
 
 @router.get("/")
 async def index(request: Request):
@@ -27,4 +41,4 @@ async def websocket_endpoint(websocket: WebSocket):
         
         for ws in websocket_list:
             if ws != websocket:
-                await ws.send_text(content)
+                await ws.send_text(caesar(content, random.randint(1, 10), alphabets))
