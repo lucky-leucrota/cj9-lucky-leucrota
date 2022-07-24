43c2cf6a2890bde5ba3c5de8dd8291aa6367d982
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2022-07-24 20:33:40 +0530

    Black Reformatted the Code!

--- src/encrypt/2_monoalpabetic_cipher.py
+++ src/encrypt/2_monoalpabetic_cipher.py
@@ -1,13 +1,15 @@
-PTA = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-CTA = ' qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789'
+PTA = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+CTA = " qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789"
+
 
 def mono_alpabetic_cipher(pt):
-    ans = ''
+    ans = ""
     for i in pt:
         try:
             ans += CTA[PTA.index(i)]
         except:
             ans += i
     return ans
+
 
 print(mono_alpabetic_cipher("ATTACKm4Once"))
