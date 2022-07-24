8292c7feeb63a616ac85fb9876a0777bd0c00d5e
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2022-07-24 20:33:40 +0530

    Black Reformatted the Code!

--- src/encrypt/1_caesar_cipher.py
+++ src/encrypt/1_caesar_cipher.py
@@ -1,16 +1,22 @@
 import string
 from random import randint
 
-def caeser_encrypt(word,key):
-    letters=string.ascii_lowercase+string.ascii_lowercase+string.ascii_uppercase+string.ascii_uppercase
-    enc=""
+
+def caeser_encrypt(word, key):
+    letters = (
+        string.ascii_lowercase
+        + string.ascii_lowercase
+        + string.ascii_uppercase
+        + string.ascii_uppercase
+    )
+    enc = ""
     for i in word:
         if i in letters:
-            enc+=letters[(letters.index(i)+key)]
+            enc += letters[(letters.index(i) + key)]
         else:
-            enc+=i
-    
+            enc += i
+
     return enc
 
 
-print(caeser_encrypt('ATTACKATm4Once',randint(1,26)))
+print(caeser_encrypt("ATTACKATm4Once", randint(1, 26)))
