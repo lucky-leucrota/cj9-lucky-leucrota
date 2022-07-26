f585b4bc908ac0b2c688fa8d7a0b23a237d20bb3
hsiam261 <hsiam261@gmail.com>
2022-07-26 18:57:11 +0600

    fix formatting

--- src/routers/algorithem.py
+++ src/routers/algorithem.py
@@ -77,11 +77,13 @@ def vigenere_decrypt(pt: str, key: str = "luckyleucrota") -> str:
     return ans
 
 
-def one_time_pad_encrypt(plain_text:bytes, key:bytes) -> bytes:
-    return bytes([p^k for p,k in zip(plain_text,key)])
+def one_time_pad_encrypt(plain_text: bytes, key: bytes) -> bytes:
+    return bytes([p ^ k for p, k in zip(plain_text, key)])
 
-def one_time_pad_decrypt(cipher_text:bytes, key:bytes) -> bytes:
-    return one_time_pad_encrypt(cipher_text,key)
+
+def one_time_pad_decrypt(cipher_text: bytes, key: bytes) -> bytes:
+    return one_time_pad_encrypt(cipher_text, key)
+
 
 def one_time_pad_keygen(length: int) -> bytes:
     return secrets.token_bytes(length)
@@ -93,19 +95,21 @@ def permutation_cipher_encrypt(plain_text: bytes, key: List[int]) -> bytes:
         cipher_text[i] = key[plain_text[i]]
     return bytes(cipher_text)
 
-def permutation_cipher_decrypt(cipher_text: bytes, key: List[int])->bytes:
-    return permutation_cipher_encrypt(cipher_text,permutation_cipher_inverse_permutation(key)) 
+
+def permutation_cipher_decrypt(cipher_text: bytes, key: List[int]) -> bytes:
+    return permutation_cipher_encrypt(
+        cipher_text, permutation_cipher_inverse_permutation(key)
+    )
+
 
 def permutation_cipher_inverse_permutation(key: List[int]) -> List[int]:
-    inverse_permutation = [0]*len(key)
+    inverse_permutation = [0] * len(key)
     for i in range(len(key)):
         inverse_permutation[key[i]] = i
     return inverse_permutation
+
 
 def permutation_cipher_gen_key() -> List[int]:
     key = [i for i in range(256)]
     random.shuffle(key)
     return key
-
-
-
