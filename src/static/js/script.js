a05fbdd57ca5c576d1937ee868c1316f220f04e7
Sasen Perera <sasen.learnings@gmail.com>
2022-07-29 12:16:03 +0530

    Added Dilsh's UI and and member join and leave and count is added.

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -3,6 +3,7 @@ function sendMessage(event, ws) {
     var input = document.getElementById("messageText");
     ws.send(input.value);
     input.value = "";
+
     event.preventDefault();
 }
 
@@ -41,8 +42,39 @@ var client_name = prompt("What is your name?");
 document.querySelector("#ws-id").textContent = client_name;
 var ws = new WebSocket(`ws://${window.location.host}/ws/${client_name}`);
 ws.onmessage = function (event) {
-    var messages = document.getElementById("messages");
-    messages.value += event.data + '\n';
+    // var messages = document.getElementById("messages");
+    // messages.value += event.data + '\n';
+    messagesContainer = document.getElementById("messages-container");
+    var chatBubble = document.createElement("div");
+    chatBubble.className = "chat-bubble";
+    let join_leave_message_check = ( event ) => {
+        let arr = event.data.split(" ");
+        if (arr[1] === "joined") {
+            let card = document.getElementById('users')
+            let user = document.createElement('p');
+            user.classList.add('card-text')
+            user.id = arr[0];
+            user.textContent = arr[0];
+            card.appendChild(user)
+        } else if (arr[1] == "left"){
+            let user = document.getElementById(arr[0]);
+            user.remove();
+        } else {
+            return;
+        }
+    }
+    join_leave_message_check(event);
+    var chatName = document.createElement("span");
+    chatName.textContent = event.data.split(":", 2)[0];
+    var chatMessage = document.createElement("p");
+    chatMessage.textContent = event.data.split(":", 2)[1];
+
+    messagesContainer.appendChild(chatBubble);
+    chatBubble.appendChild(chatName);
+    chatBubble.appendChild(chatMessage);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    document.getElementById('count').innerText = document.getElementById('users').childElementCount
+    // messagesContainer.innerHTML += event.data + '\n';
 };
 
 codeInput.registerTemplate("syntax-highlighted", codeInput.templates.prism(Prism, []));
